52529ce465d1f4479069cc4c7faa93841a75cc3b
Philip Zhang <phzhang@chapman.edu>
2026-01-20 18:51:29 -0800

    add FRED API extraction logic and initial test run

--- etl.py
+++ etl.py
@@ -6,6 +6,8 @@ import numpy as np
 import yfinance as yf
 from sqlalchemy import create_engine, text
 import logging
+import requests
+import os
 
 # Configure logging for ETL monitoring and debugging
 logging.basicConfig(
@@ -25,7 +27,57 @@ DB_CONFIG = {
 }
 
 # Step 1: Extract
-# - connect to API/CSV file, download, and return the raw dataframe 
+def extract_egg_data(series_id: str, start_date: str = '2020-01-01') -> pd.DataFrame:
+    """
+    Extract time series data from FRED API.
+
+    - Fred has a simple REST API, no official Python client needed
+    - Specifiy start_date to avoid pulling unnecessary historical data
+    
+    Args:
+        series_id: FRED series identifier
+        start_date: Start date in YYYY-MM-DD format
+
+    Resuls:
+        Dataframe with date and value columns
+    """
+
+    FRED_API_KEY = os.getenv('FRED_API_KEY')
+
+    logger.info(f"Extracting FRED series: {series_id}")
+
+    # FRED API format
+    url = 'https://api.stlouisfed.org/fred/series/observations'
+    params = {
+        'series_id' : series_id,
+        'api_key' : FRED_API_KEY,
+        'file_type' : 'json',
+        'observation_start' : start_date
+    }
+
+    try:
+        response = requests.get(url,params=params) # makes HTTP request to FRED
+        response.raise_for_status() # raises error if status != 200
+        data = response.json() # converts JSON string to Python dict
+
+        df = pd.DataFrame(data['observations'])
+        df['date'] = pd.to_datetime(df['date'])
+        df['value'] = pd.to_numeric(df['value'], errors='coerce') # handles '.' to NaN values and turning strings to int
+
+        logger.info(f"Successfully extracted {len(df)} records for {series_id}")
+
+        return df
+    
+    except Exception as e:
+        logger.error(f"Failed to extract FRED data for {series_id}: {e}")
+        raise
+
+# Testing Function
+if __name__ == "__main__":
+    df = extract_egg_data("APU0000708111") # Eggs Consumer Price Index (CPI) for U.S. city average
+    print(df.head())
+    print(f"Shape: {df.shape}")
+
 
 # Step 2: Transform
 # - aggregate the data into universal monthly 
